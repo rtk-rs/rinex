d4510f50f04975ac2ee7327420247c045f4e6f6a
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-03-30 12:19:19 +0200

    Improve Frequency and Observable support
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -188,6 +188,10 @@ pub enum FormattingError {
 /// General error (processing, analysis..)
 #[derive(Debug)]
 pub enum Error {
+    /// Invalid frequency
+    InvalidFrequency,
+    /// Unknown frequency
+    UnknownFrequency,
     /// Non supported GPS [Observable]
     UnknownGPSObservable,
     /// Non supported Galileo [Observable]
@@ -204,4 +208,20 @@ pub enum Error {
     UnknownSBASObservable,
     /// Non supported DORIS [Observable]
     UnknownDORISObservable,
+    /// Unknown GPS Frequency
+    UnknownGPSFrequency,
+    /// Unknown Galileo Frequency
+    UnknownGalileoFrequency,
+    /// Unknown QZSS Frequency
+    UnknownQzssFrequency,
+    /// Unknown Glonass Frequency
+    UnknownGlonassFrequency,
+    /// Unknown BDS Frequency
+    UnknownBDSFrequency,
+    /// Unknown IRNSS Frequency
+    UnknownIRNSSFrequency,
+    /// Unknown SBAS Frequency
+    UnknownSBASFrequency,
+    /// Unknown DORIS Frequency
+    UnknownDORISFrequency,
 }
