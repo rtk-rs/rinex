6abce8015cdfe58ea116cd78ac13c7524a3d94d4
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-05-30 10:53:39 +0200

    Add BeiDou as supported by RINEXv2..
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- src/header/parsing.rs
+++ src/header/parsing.rs
@@ -1076,11 +1076,12 @@ impl Header {
             /*
              *  Only GPS, Glonass, Galileo and SBAS are supported in V2 RINEX
              */
-            static ref KNOWN_V2_CONSTELLS: [Constellation; 4] = [
+            static ref KNOWN_V2_CONSTELLS: [Constellation; 5] = [
                 Constellation::GPS,
                 Constellation::SBAS,
                 Constellation::Glonass,
                 Constellation::Galileo,
+                Constellation::BeiDou,
             ];
         }
         let line = line.split_at(6).1;
